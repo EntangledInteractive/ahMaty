6af2c80c970fbdcd796523fa677c2e09a415c166
Nicolla Elkhouri <nicollae@entangleinteractive.com>
2017-04-06 10:06:36 +1000

    start of game with start host button

---
+++ Assets/MyHub.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyHub : MonoBehaviour {
+    private NetworkManager netManger;
+	// Use this for initialization
+	void Start () {
+        netManger = GetComponent<NetworkManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void MyStartHost()
+    {
+        Debug.Log("Starting host " + Time.timeSinceLevelLoad);
+        netManger.StartHost();
+
+    }
+
+    void OnStartHost()
+    {
+        Debug.Log("Host Start " + Time.timeSinceLevelLoad);
+    }
+}
